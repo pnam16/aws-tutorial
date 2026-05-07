84e6aa115ea0c8993bccc4f5de57268488508406
namcp <namcp@gem-corp.global>
2026-05-07 07:48:20 +0000

    May 7, 2026, 2:48 PM

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ import fs from "fs"
 const text = fs.readFileSync("SAA-C03-Question.txt", "utf-8")
 
 
-const questions = text.split("Question #")
+const questions = text.split("\nQuestion")
 
-console.log("text", text)
-// console.log("questions", questions[0])
+// console.log("text", text)
+console.log("questions 0", questions[0])
+console.log("questions 1", questions[1])
+console.log("questions 2", questions[2])
+console.log("questions 3", questions[3])
