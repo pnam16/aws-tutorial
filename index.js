d4dd4fc243d8873ca52543f1361f452a13cb04db
namcp <namcp@gem-corp.global>
2026-05-07 07:46:39 +0000

    May 7, 2026, 2:46 PM

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const text = fs.readFileSync("SAA-C03-Question.txt", "utf-8")
 
 const questions = text.split("Question #")
 
-// console.log("a", a)
-console.log("questions", questions[0])
+console.log("text", text)
+// console.log("questions", questions[0])
