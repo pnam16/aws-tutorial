95e97ddb03f0d1380355905bfde2256e0635c020
pnam <pnamhn16@gmail.com>
2026-05-11 16:07:40 +0000

    May 11, 2026, 11:07 PM

--- index.js
+++ index.js
@@ -65,4 +65,4 @@ const outputRaw = [
 const output = wrapText(outputRaw, MAX_LINE_LENGTH);
 
 fs.writeFileSync("dist/result.txt", output);
-fs.writeFileSync("dist/result.txt", output);
+fs.writeFileSync("log.txt", output);
